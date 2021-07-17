9d456268cddb623056271a7680cc63b7818278c6
Sergio Sepulveda <sergiosepulveda09@live.com>
2021-07-17 11:05:36 -0700

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Sergio Sepulveda on 2021-07-17.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
